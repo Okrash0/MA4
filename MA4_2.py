f0ddc4348a158920acb982004b1d9011bc6d7aef
Oskar <35527883+Okrash0@users.noreply.github.com>
2022-10-11 20:39:19 +0200

    Add files via upload

--- MA4_2.py
+++ MA4_2.py
@@ -4,6 +4,7 @@ from person import Person
 from time import perf_counter as pc
 from tkinter import N
 from numba import njit
+from matplotlib import pyplot as plt
 
 
 def fib_py(n):
@@ -23,18 +24,46 @@ def fib_numba(n):
 		n=n-1
 	return c
 
+def eval_time(n, f):
+	result = []
+	for i in n:
+		start = pc()
+		f(i)
+		end = pc()
+		result.append(end-start)
+	return result
+
+
 def main():
-	n = 35
+	n_start = 30
+	n_stop = 45
 	f = Person(n)
 	print(f.get())
-	f.set(7)
-	start_py = pc()
 
-	print(fib_py(n))
-
-	end_py = pc()
-	print(f"Process took {round(end_py-start_py, 2)} seconds using fib py")
 	
+	n = [i for i in range(n_start,n_stop)]
+
+	time_py = []
+	time_numba = []
+	time_cpp = []
+
+	time_py = eval_time(n, fib_py)
+	time_numba = eval_time(n, fib_numba)
+	time_cpp = eval_time(n, f.fib)
+
+	plt.plot(n,time_py)
+	plt.plot(n,time_numba)
+	plt.plot(n,time_cpp)
+
+	plt.legend(['py','numba','c++'])
+	plt.xlabel('n')
+	plt.ylabel('Time [s]')
+	plt.title('Time to calculate fib with different methods')
+	
+	plt.savefig(r'C:\Users\Brukare\Universitet\Programmeringsteknik 2\MA4\MA4_Files\time_comp')
+	plt.show()
+
+	n = 47	
 	start_numba = pc()
 
 	print(fib_numba(n))
@@ -47,7 +76,10 @@ def main():
 	print(f.fib(n))
 
 	end_py = pc()
-	print(f"Process took {round(end_py-start_py, 2)} seconds using c++")
+	print(f"Process took {round(end_py-start_py, 2)} seconds using c++") 
+
+	
+
 
 
 
